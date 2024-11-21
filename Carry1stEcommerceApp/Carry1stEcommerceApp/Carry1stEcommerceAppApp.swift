576a47cce9c42ca21c781387c9b590bd487f81c9
Ugo Val <val.ugochukwu@giglogistics.com>
2024-11-21 18:02:42 +0100

    Initial folder setup and DI package addition using SPM

---
+++ Carry1stEcommerceApp/Carry1stEcommerceApp/Carry1stEcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carry1stEcommerceAppApp.swift
+//  Carry1stEcommerceApp
+//
+//  Created by GIGL-IT on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Carry1stEcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
